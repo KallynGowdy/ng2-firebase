40442929f485f1fdad4c3cea80f5fb6cbc7ce224
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-01-05 20:43:52 -0500

    Remove dist folder

--- dist/core.js
+++
@@ -1,9 +0,0 @@
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-//export * from './src/imports';
-__export(require('./src/firebase.service'));
-__export(require('./src/firebase-utils'));
-__export(require('./src/firebase-auth.service'));
-__export(require('./src/firebase.config'));
-//# sourceMappingURL=core.js.map
