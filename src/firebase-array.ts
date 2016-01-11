88ed67c18d9392b1032b99e17b1854a19ce0a4c0
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-01-10 22:57:28 -0500

    Add Polyfill for findIndex()

--- src/firebase-array.ts
+++ src/firebase-array.ts
@@ -31,6 +31,30 @@ if (!Array.prototype.find) {
     };
 }
 
+// Polyfill for Array.findIndex()
+if (!Array.prototype.findIndex) {
+    Array.prototype.findIndex = function (predicate) {
+        if (this === null) {
+            throw new TypeError('Array.prototype.findIndex called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list:any = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return i;
+            }
+        }
+        return -1;
+    };
+}
+
 /**
  * Defines a class that provides capabilities to synchronize ordered lists for a Firebase Object.
  *
